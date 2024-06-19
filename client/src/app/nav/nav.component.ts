fc2e39e66a967e0b284f38429cef10d1007f29d2
Darrus Goh <darrusgoh@gmail.com>
2024-06-19 23:24:08 +0800

    Section 3 & Course Update

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,33 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../services/account.service';
-import { BsDropdownModule} from 'ngx-bootstrap/dropdown';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [FormsModule, BsDropdownModule],
+  imports: [FormsModule, BsDropdownModule, AsyncPipe],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css',
 })
-export class NavComponent implements OnInit {
-  loggedIn = false;
-
-  constructor(private accountService: AccountService) {}
-
-  ngOnInit(): void {
-    this.getCurrentUser();
-  }
-
-  getCurrentUser() {
-    this.accountService.currentUser$.subscribe({
-      next: user => this.loggedIn = !!user,
-      error: error => console.log(error)
-    });
-  }
+export class NavComponent {
+  model: any = {};
+  constructor(public accountService: AccountService) {}
 
   login() {
-    this.accountService.login({});
+    this.accountService.login(this.model).subscribe({
+      next: (response) => {
+        console.log(response);
+      },
+      error: (error) => {
+        console.error(error);
+      },
+    });
   }
 
   logout() {
